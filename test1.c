6785800d4a9531707b8d1856099306ffe31a0135
shh2000 <13820618441@163.com>
2019-01-31 14:05:52 +0800

    first

---
+++ test1.c
@@ -0,0 +1,31 @@
+/*
+ Author: 石浤澔
+ Result: AC	Submission_id: 977312
+ Created at: Wed Oct 10 2018 22:14:51 GMT+0800 (CST)
+ Problem_id: 1454	Time: 1513	Memory: 11784
+*/
+
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+#include<stdlib.h>
+
+int bar[10000004];
+
+int main() {
+    int n;
+    while (scanf("%d", &n) == 1) {
+        n++;
+        int buffer;
+        while (n--) {
+            scanf("%d", &buffer);
+            bar[buffer]++;
+            if (bar[buffer] == 2) {
+                printf("%d\n", buffer);
+            }
+        }
+        memset(bar, 0, 10000004);
+    }
+    return 0;
+}
---
+++ tool.h
@@ -0,0 +1,12 @@
+//
+// Created by admin on 2019/1/31.
+//
+
+#ifndef P0_TOOL_H
+#define P0_TOOL_H
+
+#endif //P0_TOOL_H
+
+int sum(int a, int b) {
+    return a + b;
+}
